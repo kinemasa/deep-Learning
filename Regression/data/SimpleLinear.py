5c90f2f5645f210405936f2f6ba53f218d8cf2ed
Masaya Kinefuchi <kine0718@icloud.com>
2023-04-23 13:26:44 +0900

    Linear Regression

---
+++ Regression/data/SimpleLinear.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+##import dataset
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+## set train ,test  data
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+##train
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()##最小二乗法
+regressor.fit(X_train, y_train)##最小二乗法を用いて切片と係数を求める
+
+##show results of train
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+##show results 
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
